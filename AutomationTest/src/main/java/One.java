1531bf9ea50fab4f5ed7bae7a7bd1c0577f5a50b
Prasant <princebit3828@gmail.com>
2017-09-03 19:11:33 +0530

    first chrome test

--- AutomationTest/src/main/java/One.java
+++ AutomationTest/src/main/java/One.java
@@ -1,9 +1,26 @@
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class One {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("Hello Moto");
-	}
+
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://www.google.com");
+		if (driver.findElement(By.xpath("//input[@name='q']")).isEnabled()) {
+			System.out.println("Google search text box Is enabled.");
+			driver.findElement(By.xpath("//input[@name='q']")).sendKeys("WebDriver Test successful.");
+			driver.findElement(By.xpath("//button[@name='btnG']")).click();
+			System.out.println("Google search completed.");
+		} else {
+			System.out.println("Google search test box Is Not enabled.");
+		}
+		driver.close();
+		}
 
 }
